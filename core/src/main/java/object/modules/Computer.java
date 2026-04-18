99821c27ffea74f936d62a738c4be37d6c615d24
lllnightlll <playpr991@gmail.com>
2026-04-18 16:25:40 +0600

    computer class

--- core/src/main/java/object/modules/Computer.java
+++ core/src/main/java/object/modules/Computer.java
@@ -1,5 +1,16 @@
 package object.modules;
 
 public class Computer {
+    private String text;
 
+    Computer (String text) {
+        this.text = text;
+    }
+
+    public boolean check (String x) {
+        if ((x.toUpperCase()).equals((this.text).toUpperCase())) {
+            return true;
+        }
+        return false;
+    }
 }
