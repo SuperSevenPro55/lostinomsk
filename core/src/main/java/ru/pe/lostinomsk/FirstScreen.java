9973ad7388e35756c1eaf7698e059f29b7b47928
lllnightlll <playpr991@gmail.com>
2026-04-18 10:56:45 +0600

    background

--- core/src/main/java/ru/pe/lostinomsk/FirstScreen.java
+++ core/src/main/java/ru/pe/lostinomsk/FirstScreen.java
@@ -1,17 +1,39 @@
 package ru.pe.lostinomsk;
 
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 
 /** First screen of the application. Displayed after the application is created. */
 public class FirstScreen implements Screen {
+    private Texture backgroundTexture;
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+
     @Override
     public void show() {
         // Prepare your screen here.
+        backgroundTexture = new Texture("room.png");
+        spriteBatch = new SpriteBatch();
+        viewport = new FitViewport(8, 5);
     }
 
     @Override
     public void render(float delta) {
         // Draw your screen here. "delta" is the time since last render in seconds.
+        // organize code into three methods
+        // input();
+        // logic();
+        draw();
     }
 
     @Override
@@ -19,6 +41,7 @@ public class FirstScreen implements Screen {
         // If the window is minimized on a desktop (LWJGL3) platform, width and height are 0, which causes problems.
         // In that case, we don't resize anything, and wait for the window to be a normal size before updating.
         if(width <= 0 || height <= 0) return;
+        viewport.update(width, height, true); // true centers the camera
 
         // Resize your screen here. The parameters represent the new window size.
     }
@@ -41,5 +64,20 @@ public class FirstScreen implements Screen {
     @Override
     public void dispose() {
         // Destroy screen's assets here.
+    }
+
+    private void draw() {
+        ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        // store the worldWidth and worldHeight as local variables for brevity
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight); // draw the background
+
+        spriteBatch.end();
     }
 }
