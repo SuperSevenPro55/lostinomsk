9068821fdbc81ca5939aa1e75e76829ff31f9b57
Roman <romanplotskiy@gmail.com>
2026-04-18 13:55:17 +0600

    Main Menu created (without action by pressing button)

--- core/src/main/java/ru/pe/lostinomsk/screens/MainMenuScreen.java
+++ core/src/main/java/ru/pe/lostinomsk/screens/MainMenuScreen.java
@@ -2,27 +2,69 @@ package ru.pe.lostinomsk.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.ScreenUtils;
+import ru.pe.lostinomsk.FirstScreen;
 import ru.pe.lostinomsk.Main;
 
 public class MainMenuScreen implements Screen {
     private final Main game;
     private BitmapFont font;
+    private SpriteBatch batch;
+    private Texture background;
+
+    private Rectangle playButtonBounds;
+    private Vector3 touchPoint;
+
 
     public MainMenuScreen(Main game, BitmapFont font) {
         this.game = game;
+        this.font = font;
     }
 
     @Override
     public void show() {
-        // Шрифт
+        batch = new SpriteBatch();
 
+        font.getData().setScale(3f);
+
+        background = new Texture("main_menu_background.png");
+        playButtonBounds = new Rectangle(Gdx.graphics.getWidth() / 2f - 100, Gdx.graphics.getHeight() / 2f - 50, 200, 100);
+        touchPoint = new Vector3();
     }
 
     @Override
     public void render(float delta) {
+        ScreenUtils.clear(Color.BLACK);
 
+        // Фон
+        batch.begin();
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        // Название
+        font.setColor(Color.WHITE);
+        font.draw(batch,
+            "Lost in Omsk",
+            Gdx.graphics.getWidth() / 2f - 250,
+            Gdx.graphics.getHeight() / 2f + 80);
+
+        // Кнопка "Играть"
+        font.setColor(Color.WHITE);
+        font.draw(batch, "ИГРАТЬ", playButtonBounds.x - 10, playButtonBounds.y - 300);
+
+        batch.end();
+
+        if (Gdx.input.justTouched()) {
+            touchPoint.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+            if (playButtonBounds.contains(touchPoint.x, Gdx.graphics.getHeight())) {
+                game.setScreen(new FirstScreen());
+            }
+        }
     }
 
     @Override
@@ -42,11 +84,13 @@ public class MainMenuScreen implements Screen {
 
     @Override
     public void hide() {
-
+        dispose();
     }
 
     @Override
     public void dispose() {
+        batch.dispose();
+        background.dispose();
         font.dispose();
     }
 }
